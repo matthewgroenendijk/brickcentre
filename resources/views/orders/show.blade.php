908534e6c14d8103d397e7cdf5fe3606e80c92da
fritsmeister12 <1034749@hr.nl>
2023-04-05 13:33:11 +0200

    Order show visual changes

--- resources/views/orders/show.blade.php
+++ resources/views/orders/show.blade.php
@@ -31,7 +31,7 @@
     </header>
     <div class="grid-cols-3 grid gap-4 mb-4">
         <div
-            class="relative flex flex-col min-w-0 break-words bg-slate-700 shadow-xl dark:bg-slate-850 dark:shadow-dark-xl rounded-2xl bg-clip-border">
+            class="relative flex flex-col min-w-0 break-words bg-gray-50 dark:bg-slate-700 border-2 border-gray-200 dark:shadow-dark-xl rounded-2xl bg-clip-border">
             <div class="flex-auto py-4 px-2">
                 <div class="flex flex-row">
                     <div class="flex-none w-2/3 max-w-full px-3">
@@ -55,7 +55,7 @@
             </div>
         </div>
         <div
-            class="relative flex flex-col min-w-0 break-words bg-slate-700 shadow-xl dark:bg-slate-850 dark:shadow-dark-xl rounded-2xl bg-clip-border">
+            class="relative flex flex-col min-w-0 break-words bg-gray-50 dark:bg-slate-700 border-2 border-gray-200 dark:shadow-dark-xl rounded-2xl bg-clip-border">
             <div class="flex-auto py-4 px-2">
                 <div class="flex flex-row">
                     <div class="flex-none w-2/3 max-w-full px-3">
@@ -79,7 +79,7 @@
             </div>
         </div>
         <div
-            class="relative flex flex-col min-w-0 break-words bg-slate-700 shadow-xl dark:bg-slate-850 dark:shadow-dark-xl rounded-2xl bg-clip-border">
+            class="relative flex flex-col min-w-0 break-words bg-gray-50 dark:bg-slate-700 border-2 border-gray-200 dark:shadow-dark-xl rounded-2xl bg-clip-border">
             <div class="flex-auto py-4 px-2">
                 <div class="flex flex-row">
                     <div class="flex-none w-2/3 max-w-full px-3">
@@ -106,10 +106,10 @@
 
             <div class="w-full max-w-full px-3 shrink-0 md:w-8/12 md:flex-0">
                 <div
-                    class="relative flex flex-col min-w-0 break-words bg-white dark:bg-slate-700 border-0 shadow-xl dark:bg-slate-850 dark:shadow-dark-xl rounded-2xl bg-clip-border">
+                    class="relative flex flex-col min-w-0 break-words bg-gray-50 dark:bg-slate-700 border-2 border-gray-200 dark:bg-slate-850 dark:shadow-dark-xl rounded-2xl bg-clip-border">
                     <div class="border-black/12.5 rounded-t-2xl border-b-0 border-solid py-4 px-6 pb-0">
                         <div class="flex items-center">
-                            <p class="mb-0 dark:text-white/80">Klant Gegevens</p>
+                            <p class="mb-0 text-lg font-bold dark:text-white/80">Klant Gegevens</p>
                             <button type="button"
                                 class="inline-block px-8 py-2 mb-4 ml-auto font-bold leading-normal text-center text-white align-middle transition-all ease-in bg-yellow-500 border-0 rounded-lg shadow-md cursor-pointer text-xs tracking-tight-rem hover:shadow-xs hover:-translate-y-px active:opacity-85">Wijzig</button>
                         </div>
@@ -183,7 +183,7 @@
             </div>
             <div class="w-full max-w-full px-3 lg:w-1/3 lg:flex-none">
                 <div
-                    class="relative flex flex-col pb-2 h-content min-w-0 break-words bg-white dark:bg-slate-700 border-0 border-transparent border-solid shadow-xl dark:bg-slate-850 dark:shadow-dark-xl rounded-2xl bg-clip-border">
+                    class="relative flex flex-col pb-2 h-content min-w-0 break-words border-2 border-gray-200 bg-gray-50 dark:bg-slate-700 dark:bg-slate-850 dark:shadow-dark-xl rounded-2xl bg-clip-border">
                     <div class="p-4 pb-0 mb-0 border-b-0 border-b-solid rounded-t-2xl border-b-transparent">
                         <div class="flex flex-wrap -mx-3">
                             <div class="flex items-center flex-none w-1/2 max-w-full px-3">
